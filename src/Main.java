ea78445818c8a1ceaed6fc5f6177db67dfe12df4
milalina <ludmilla.mamelina@gmail.com>
2019-12-03 15:49:11 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product p1 = new Product("vacuum cleaner", 100);
+        Product p2 = new Product("pencil", 3);
+        Product p3 = new Product("bike", 300);
+        Product p4 = new Product("pencil", 3);
+        Product p5 = new Product("pencil", 3);
+        Product p6 = new Product("pencil", 3);
+        Product p7 = new Product("bike", 300);
+        System.out.println(p1);
+
+        ShoppingCart shoppingCart=new ShoppingCart();
+        shoppingCart.fillShoppingCart(p1);
+        shoppingCart.fillShoppingCart(p2);
+        shoppingCart.fillShoppingCart(p3);
+        shoppingCart.fillShoppingCart(p4);
+        shoppingCart.fillShoppingCart(p5);
+        shoppingCart.fillShoppingCart(p6);
+        shoppingCart.fillShoppingCart(p7);
+
+        System.out.println(shoppingCart.calculateTotalPrice());
+        System.out.println("Products in the cart are "+ shoppingCart.getProducts());
+    }
+}
---
+++ src/ShoppingCart.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class ShoppingCart {
+    private double totalPrice;
+
+    List<Product> products=new ArrayList<>();
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public ShoppingCart() {
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public List<Product> fillShoppingCart(Product product){
+        products.add(product);
+        return products;
+    }
+
+    public Map<String, Integer> howManySameProducts(){
+        List<String> productNamesList =new ArrayList<>();
+        for(Product product: products){
+            productNamesList.add(product.getName());
+        }
+        Map<String, Integer> hm = new HashMap<String, Integer>();
+
+        for (String i : productNamesList) {
+            Integer j = hm.get(i);
+            hm.put(i, (j == null) ? 1 : j + 1);
+        }
+        System.out.println(hm);
+        return hm;
+    }
+
+    public double calculateTotalPrice(){
+        String productInThreePlusQuantity;
+      /* for (Map.Entry<String, Integer> val : howManySameProducts().entrySet()) {
+            System.out.println("Element " + val.getKey() + " "
+                    + "occurs"
+                    + ": " + val.getValue() + " times");
+        }*/
+
+        for(Product product: products){
+            totalPrice+=product.getPrice();
+        }
+        if(products.size()>=5){
+            totalPrice=totalPrice*0.9;
+        }
+        return totalPrice;
+    }
+
+}
